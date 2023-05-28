08b236c810c1ef5c700e640c56f56c4dac82dca8
Tamirucode <tam.egu76@gmail.com>
2023-05-28 22:05:59 +0000

    add signup fronted devlopment

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,4 +5,12 @@ from taskmanager.models import User, Admindata, Tabledata
 
 @app.route("/")
 def home():
+    return render_template("base.html")
+
+@app.route("/signup")
+def signup():
     return render_template("signup.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
