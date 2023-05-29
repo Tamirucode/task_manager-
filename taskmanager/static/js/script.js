7d62e0f3a5ee30eee32dc51c953071d4846a93fe
Tamirucode <tam.egu76@gmail.com>
2023-05-29 16:58:28 +0000

    add template to taskmanager

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,11 +1,12 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav= document.querySelectorAll('.sidenav');
-     M.Sidenav.init(elems, options);
+     M.Sidenav.init(sidenav);
   });
 // Or with jQuery
 
 $(document).ready(function(){
     $('.sidenav').sidenav();
-  });
-        
+  
+    $("#copyright").text(new Date().getFullYear());
+});      
